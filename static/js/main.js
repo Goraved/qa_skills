b25c82b5894701f04e5280736c2dbdaad2f897aa
Roman Pobotin <rpobotin@adyax.com>
2018-05-29 21:27:21 +0300

    small fix

--- static/js/main.js
+++ static/js/main.js
@@ -41,7 +41,7 @@ $(function(){
 			data: $('form').serialize(),
 			type: 'POST',
 			success: function(response){
-				console.log(response);
+				window.location.href = "/statistics"
 			},
 			error: function(error){
 				console.log(error);
