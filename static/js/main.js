ef5c5a930dd34ea8667b0813e4fbdb4e9f60cc3f
Roman Pobotin <rpobotin@adyax.com>
2019-12-15 23:35:31 +0200

    Fix timer 2

--- static/js/main.js
+++ static/js/main.js
@@ -55,7 +55,7 @@ function scrollFunction() {
 var ind = 0;
 function poll(id) {
     $.get('/TaskStatus/'+id, function(result) {
-        if(result != 'True' && ind < 3) {
+        if(result != 'True' && ind < 60) {
             ind++;
             $("#wait").css("display", "block");
             $("#analyze").prop( "disabled", true );
