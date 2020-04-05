63b623427f71ec1edbe2b890be27047e0aeb6a67
Goraved <roman.pobotin@gmail.com>
2020-04-05 13:05:03 +0300

    Fix graph opening

--- static/js/main.js
+++ static/js/main.js
@@ -63,6 +63,7 @@ window.onclick = function(event) {
 $('a[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
+    .not('[href="#graph_img"]')
     .not('[href="#0"]')
     .click(function(event) {
         // On-page links
